fecd1296f84bf42b7230e2633f59bf88cdf764a8
Joseph Kim <gyuhankim@gmail.com>
2018-07-24 14:43:15 -0400

    initial commit

---
+++ big-o-drills.js
@@ -0,0 +1,95 @@
+// 1) EVEN OR ODD
+//   a) What is the algorithm trying to achieve?
+//     Determine if the input value is an even or odd number.
+
+//   b) Algorithmic complexity?
+//     O(1)
+
+//   c) How was the algorithmic complexity determined?
+//     Performs arithmetic once to determine output.
+
+
+// 2) ARE YOU HERE?
+//   a) What is the algorithm trying to achieve? 
+//     Finds which of the values from arr1 exist in arr2.
+
+//   b) Algorithmic complexity?
+//     O(n^2)
+
+//   c) How was the algorithmic complexity determined?
+//     The first loop iterates through arr1 setting el1 as the value of arr1[index] and for each iteration a second loop is iterating through arr2 setting el2 as the value of arr2[index] and comparing if el1 === el2.
+
+
+// 3) DOUBLER
+//   a) What is the algorithm trying to achieve?
+//     Doubling the value of each element in Array.
+
+//   b) Algorithmic complexity?
+//     O(n)
+
+//   c) How was the algorithmic complexity determined?
+//     Iterating through the length of the input (array).
+
+
+// 4) NAIVE SEARCH
+//   a) What is the algorithm trying to achieve?
+//     Finding the matching array element to the input.
+
+//   b) Algorithmic complexity?
+//     O(n)
+
+//   c) How was the algorithmic complexity determined?
+//     Iterating through the length of the input (array).
+
+// 5) CREATING PAIRS
+//   a) What is the algorithm trying to achieve?
+//     Receive an input (array) and log out pairs of arr[i] and arr[j].
+
+//   b) Algorithmic complexity?
+//     O(n^2)
+
+//   c) How was the algorithmic complexity determined?
+//     Iterating through each element of the array and for each element loop through the array again and pair the element[index] with element[index + 1].
+
+
+// 6) COMPUTING FIBONACCIS
+//   a) What is the algorithm trying to achieve?
+//     Creating an array of fibonacci numbers.
+
+//   b) Algorithmic complexity?
+//     O(n)
+
+//   c) How was the algorithmic complexity determined?
+//     The for-loop runs n (input) amount of times.
+
+// 7) AN EFFICIENT SEARCH
+//   a) What is the algorithm trying to achieve?
+//     A binary search (on, hopefully, a sorted array).
+
+//   b) Algorithmic complexity?
+//     O(log n)
+
+//   c) How was the algorithmic complexity determined?
+//     With each iteration, the length of the input is shortened (by half).
+
+
+// 8) RANDOM ELEMENT
+//   a) What is the algorithm trying to achieve?
+//     Return a random element in an array.
+
+//   b) Algorithmic complexity?
+//     O(1)
+
+//   c) How was the algorithmic complexity determined?
+//     The function contains a single line of code that randomizes the index value of the array element and returns it.
+
+
+// 9) IS IT PRIME?
+//   a) What is the algorithm trying to achieve?
+//     Determine if the input is a prime number.
+
+//   b) Algorithmic complexity?
+//     O(n)
+
+//   c) How was the algorithmic complexity determined?
+//     The function iterates n (input) number of times.
